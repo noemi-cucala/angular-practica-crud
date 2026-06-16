139d10a90fc8a089151c14532a2f6247708f5f40
noemi-cucala <noemi.cucalaverges@emeal.nttdata.com>
2026-06-16 09:31:24 +0200

    Basic routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePage,
+    },
+    /*{
+        path: 'cars/list',
+        component:, 
+    },*/
+    //{path: '**', component: NotFound}, // Wildcard - always last
+];
